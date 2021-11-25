42ae1207f52d1e5da5796df72e554a57c3ac2dba
heicaijun <972136600@qq.com>
2021-11-25 16:49:56 +0800

    update

--- 02.完整项目/Spinach/src/main/java/cn/heicaijun/spinach/SpinachApplication.java
+++ 02.完整项目/Spinach/src/main/java/cn/heicaijun/spinach/SpinachApplication.java
@@ -3,6 +3,10 @@ package cn.heicaijun.spinach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主程序类
+ * @SpringBootApplication： 这是一个SpringBoot应用
+ */
 @SpringBootApplication
 public class SpinachApplication {
 
--- 02.完整项目/Spinach/src/main/java/cn/heicaijun/spinach/controller/Index.java
+++ 02.完整项目/Spinach/src/main/java/cn/heicaijun/spinach/controller/Index.java
@@ -1,15 +1,12 @@
 package cn.heicaijun.spinach.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Index {
 
     @GetMapping(value = {"/","/index"})
-    public String show(Model model){
-        model.addAttribute("name", "陈世豪");
+    public String show(){
         return "index";
-
     }
 }
